39eb0d3be537985b2e48544e3f828241284e5916
garbergc <clare.garberg@gmail.com>
2022-06-24 11:58:59 -0400

    changes

--- RenamingImages.py
+++ RenamingImages.py
@@ -12,13 +12,18 @@ root = "/Users/claregarberg/Documents/Graduate School/Summer 2022/Image Analytic
 
 os.chdir(root)
 
+count = 0
+
 for dir, subdirs, files in os.walk("."):
     for i, f in enumerate(files):
         if f == ".DS_Store":
             continue
         else:
             folder_name = dir[2:]
-            name_new = folder_name + str(i)
+            name_new = folder_name + str(i) + ".jpg"
             path_new = root + "/" + folder_name
+            count = count + 1
             #print(name_new)
             os.rename(os.path.join(path_new, f), os.path.join(path_new, name_new))
+
+print(count)
